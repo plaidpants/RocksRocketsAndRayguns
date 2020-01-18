378e1f17b3f7e346e85eb35a5a6dcebffec9401b
JWS <james.surine@gmail.com>
2020-01-18 13:00:27 -0600

    add some missing files and .meta files

---
+++ Assets/MyMouseLook.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// MouseLook rotates the transform based on the mouse delta.
+/// Minimum and Maximum values can be used to constrain the possible rotation
+
+/// To make an FPS style character:
+/// - Create a capsule.
+/// - Add a rigid body to the capsule
+/// - Add the MouseLook script to the capsule.
+///   -> Set the mouse look to use LookX. (You want to only turn character but not tilt it)
+/// - Add FPSWalker script to the capsule
+
+/// - Create a camera. Make the camera a child of the capsule. Reset it's transform.
+/// - Add a MouseLook script to the camera.
+///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class MyMouseLook : MonoBehaviour
+{
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float sensitivity = 0.1f;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationX = 0F;
+    float rotationY = 0F;
+
+    Quaternion originalRotation;
+
+    void Update()
+    {
+#if !UNITY_ANDROID || UNITY_EDITOR
+        // Read the mouse input axis
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+        rotationX = ClampAngle(rotationX, minimumX, maximumX);
+        rotationY = ClampAngle(rotationY, minimumY, maximumY);
+
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+
+        transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+#endif
+        if (Input.GetKey(KeyCode.W) || Input.GetButton("Fire2"))
+        {
+            transform.position += Camera.main.transform.forward * sensitivity * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= Camera.main.transform.forward * sensitivity * Time.deltaTime;
+        }
+        /*
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Camera.main.transform.right * sensitivity * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Camera.main.transform.right * sensitivity * Time.deltaTime;
+        }
+        */
+    }
+
+    void Start()
+    {
+#if !UNITY_ANDROID || UNITY_EDITOR
+        // Make the rigid body not change rotation
+        originalRotation = transform.localRotation;
+#endif
+    }
+
+#if !UNITY_ANDROID || UNITY_EDITOR
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+         angle += 360F;
+        if (angle > 360F)
+         angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+#endif
+}
---
+++ Assets/PlayerTracker.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTracker : MonoBehaviour
+{
+    public RawImage OnScreenSprite;
+    public Canvas screenCanvas;
+    public Text debugText;
+    public static GameObject trackingObject;
+    public float xoffset;
+    public float yoffset;
+
+    public static void SetTrackingObject(GameObject track)
+    {
+        trackingObject = track;
+    }
+
+    void Start()
+    {
+        GetComponent<Canvas>().worldCamera = Camera.main;
+        OnScreenSprite.enabled = false;
+        debugText.enabled = false;
+    }
+
+    void Update()
+    {
+        if (trackingObject != null)
+        {
+            Vector3 screenMiddle = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+
+            Vector3 screenpos = Camera.main.WorldToScreenPoint(trackingObject.transform.position);
+
+            float tarAngle = (Mathf.Atan2(screenpos.x - screenMiddle.x, Screen.height - screenpos.y - screenMiddle.y) * Mathf.Rad2Deg) + 90;
+            if (tarAngle < 0)
+            {
+                tarAngle += 360;
+            }
+
+            //debugText.text = Mathf.RoundToInt(tarAngle).ToString() + " " + Mathf.RoundToInt(screenpos.x).ToString() + " " + Mathf.RoundToInt(screenpos.y).ToString() + " " + Mathf.RoundToInt(screenpos.z).ToString();
+            debugText.text = "";
+
+            if (/*(screenpos.x > Screen.width) || (screenpos.y > Screen.height) || (screenpos.y < 0) || (screenpos.x < 0) || */ (screenpos.z < 0))
+            {
+                OnScreenSprite.enabled = true;
+
+                OnScreenSprite.material.SetFloat("_Alpha", 0.5f);
+            }
+            else
+            {
+                OnScreenSprite.enabled = false;
+            }
+
+            if (screenpos.z < 0)
+            {
+                screenpos = new Vector3(Screen.width / 2, Screen.height / 2, screenpos.z);
+                tarAngle = tarAngle - 180;
+            }
+
+            if (screenpos.x > Screen.width - Screen.width * xoffset)
+            {
+                screenpos.x = Screen.width - Screen.width * xoffset;
+            }
+            else if (screenpos.x < Screen.width * xoffset)
+            {
+                screenpos.x = Screen.width * xoffset;
+            }
+            if (screenpos.y > Screen.height - Screen.height * yoffset)
+            {
+                screenpos.y = Screen.height - Screen.height * yoffset;
+            }
+            else if (screenpos.y < Screen.height * yoffset)
+            {
+                screenpos.y = Screen.height * yoffset;
+            }
+
+            RectTransformUtility.ScreenPointToWorldPointInRectangle(screenCanvas.GetComponent<RectTransform>(), screenpos, Camera.main, out screenpos);
+
+            OnScreenSprite.rectTransform.position = screenpos;
+            OnScreenSprite.transform.rotation = Camera.main.transform.rotation * Quaternion.Euler(0, 0, tarAngle);
+        }
+        else
+        {
+            OnScreenSprite.enabled = false;
+        }
+    }
+}
